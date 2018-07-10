1da932f34a4e6695d98e777b5e1114dc7bf2c928
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2018-07-10 12:38:46 +0300

    NODE-934 Shutdown will not lead to dead letters

--- src/main/scala/com/wavesplatform/matcher/Matcher.scala
+++ src/main/scala/com/wavesplatform/matcher/Matcher.scala
@@ -21,6 +21,7 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.reflect.runtime.universe._
 import akka.pattern.ask
+import akka.util.Timeout
 
 class Matcher(actorSystem: ActorSystem,
               wallet: Wallet,
@@ -53,7 +54,8 @@ class Matcher(actorSystem: ActorSystem,
   @volatile var matcherServerBinding: ServerBinding = _
 
   def shutdownMatcher(): Unit = {
-    Await.result(matcher ? MatcherActor.Shutdown, 5.minute)
+    implicit val timeout: Timeout = Timeout.durationToTimeout(5.minute)
+    Await.result(matcher ? MatcherActor.Shutdown, timeout.duration)
     db.close()
     Await.result(matcherServerBinding.unbind(), 10.seconds)
   }
